64d6b38d45dcbebf1a7c2c7f6be0e86caf4ea21d
alpari01 <alan.parik456@gmail.com>
2023-06-20 16:41:48 +0300

    Add basic table

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,11 @@ module.exports = {
   mode: 'development',
   devtool: 'source-map',
   context: path.resolve(__dirname, 'src'),
-  entry: './scss/main.scss',
+  entry: {
+    reset: './scss/reset.scss',
+    main: './scss/main.scss',
+    meal_plan: './scss/meal-plan.scss',
+  },
   module: {
     rules: [
       {
