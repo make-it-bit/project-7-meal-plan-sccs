c81652f4f7a9ce8c5c5b995b39917bf620d222c4
alpari01 <alan.parik456@gmail.com>
2023-06-22 13:18:53 +0300

    Add edanam api fetch script

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -68,48 +68,68 @@ const hideErrorMessage = () => {
 };
 
 const calculateCalories = (gender, height, weight, age, activityLevel) => {
-  let bmrCoefficent = 0;
-  activityLevel = activityLevel.toLowerCase();
-  switch (activityLevel) {
-    case 'sedentary':
-      bmrCoefficent = 1.2;
-      break;
-    case 'lightly active':
-      bmrCoefficent = 1.375;
-      break;
-    case 'active':
-      bmrCoefficent = 1.55;
-      break;
-    case 'very active':
-      bmrCoefficent = 1.725;
-      break;
-    case 'extremely active':
-      bmrCoefficent = 1.9;
-      break;
-  }
+  if (gender && height && weight && age && activityLevel) {
+    let bmrCoefficent = 0;
+    activityLevel = activityLevel.toLowerCase();
+    switch (activityLevel) {
+      case 'sedentary':
+        bmrCoefficent = 1.2;
+        break;
+      case 'lightly active':
+        bmrCoefficent = 1.375;
+        break;
+      case 'active':
+        bmrCoefficent = 1.55;
+        break;
+      case 'very active':
+        bmrCoefficent = 1.725;
+        break;
+      case 'extremely active':
+        bmrCoefficent = 1.9;
+        break;
+    }
 
-  if (gender === 'male') {
-    return (
-      (66.5 + 13.75 * weight + 5.003 * height - 6.75 * age) * bmrCoefficent
-    );
-  }
-  if (gender === 'female') {
-    return (
-      (655.1 + 9.563 * weight + 1.85 * height - 4.676 * age) * bmrCoefficent
-    );
+    if (gender === 'male') {
+      return (
+        (66.5 + 13.75 * weight + 5.003 * height - 6.75 * age) * bmrCoefficent
+      );
+    }
+    if (gender === 'female') {
+      return (
+        (655.1 + 9.563 * weight + 1.85 * height - 4.676 * age) * bmrCoefficent
+      );
+    }
   }
 };
 
-const getRecepies = async (ingredients) => {
+const getRecepies = async () => {
   const APP_ID = 'c34ab5d9';
   const APP_KEY = 'a7a5284e0132e033649dbbd050765bf7';
   const URL_BASE = 'https://api.edamam.com/api/recipes/v2?type=public';
 
-  const caloriesAmount = calculateCalories();
+  // const caloriesAmount = calculateCalories(
+  //   getUserInputGender(),
+  //   getUserInputHeight(),
+  //   getUserInputWeight(),
+  //   getUserInputAge(),
+  //   getUserInputActivity()
+  // );
+  caloriesAmount = 2000;
+  const ingredients = 'chicken salad potato';
 
-  const url = `${URL_BASE}&q=${encodeURIComponent(
-    ingredients
-  )}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=Breakfast&calories=${caloriesAmount}`;
+  if (caloriesAmount) {
+    const url = `${URL_BASE}&q=${encodeURIComponent(
+      ingredients
+    )}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=Breakfast&calories=${caloriesAmount}`;
 
-  console.log(url);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      hideErrorMessage();
+      console.log(data);
+      return data;
+    } catch (error) {
+      showErrorMessage(`Could not get data`);
+    }
+  }
 };
