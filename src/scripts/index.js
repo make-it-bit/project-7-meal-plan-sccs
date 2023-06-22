33dfac2c460987943eb6bd8b096f633bc14f6d53
alpari01 <alan.parik456@gmail.com>
2023-06-22 12:46:49 +0300

    Add form fields validation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,16 +9,64 @@ const handleUserFormInput = () => {
     .getElementById('activity-levels')
     .querySelector('input[type="radio"]:checked').value;
   const ingredients = document.getElementById('ingredients');
-  // TODO: add validations for all fields (null check, format check)
-
-  const caloriesAmount = calculateCalories(
-    inputGender,
-    inputHeight,
-    inputWeight,
-    inputAge,
-    inputAcvity
-  );
 };
+
+const getUserInputHeight = () => {
+  const inputHeight = document.getElementById('input-height').value;
+
+  if (!Number(inputHeight)) {
+    showErrorMessage('Height must be a numeric value');
+  } else if (inputHeight < 0) {
+    showErrorMessage('Height must be greater than 0');
+  } else return inputHeight;
+};
+
+const getUserInputWeight = () => {
+  const inputWeight = document.getElementById('input-weight').value;
+
+  if (!Number(inputWeight)) {
+    showErrorMessage('Weight must be a numeric value');
+  } else if (inputWeight < 0) {
+    showErrorMessage('Weight must be greater than 0');
+  } else return inputWeight;
+};
+
+const getUserInputAge = () => {
+  const inputAge = document.getElementById('input-age').value;
+
+  if (!Number(inputAge)) {
+    showErrorMessage('Age must be a numeric value');
+  } else if (inputAge < 0) {
+    showErrorMessage('Age must be greater than 0');
+  } else return inputAge;
+};
+
+const getUserInputGender = () => {
+  return document
+    .getElementById('genders')
+    .querySelector('input[type="radio"]:checked').value;
+};
+
+const getUserInputActivity = () => {
+  return document
+    .getElementById('activity-levels')
+    .querySelector('input[type="radio"]:checked').value;
+};
+
+const getUserInputIngredients = () => {
+  return document.getElementById('ingredients');
+};
+
+const showErrorMessage = (message) => {
+  const errorContainer = document.getElementById('error-container');
+  errorContainer.style.display = 'block';
+  errorContainer.innerHTML = message;
+};
+
+const hideErrorMessage = () => {
+  document.getElementById('error-container').style.display = 'none';
+};
+
 const calculateCalories = (gender, height, weight, age, activityLevel) => {
   let bmrCoefficent = 0;
   activityLevel = activityLevel.toLowerCase();
@@ -44,17 +92,20 @@ const calculateCalories = (gender, height, weight, age, activityLevel) => {
     return (
       (66.5 + 13.75 * weight + 5.003 * height - 6.75 * age) * bmrCoefficent
     );
-  } else {
+  }
+  if (gender === 'female') {
     return (
       (655.1 + 9.563 * weight + 1.85 * height - 4.676 * age) * bmrCoefficent
     );
   }
 };
 
-const getRecepies = async (caloriesAmount, ingredients) => {
-  const APP_ID = c34ab5d9;
-  const APP_KEY = a7a5284e0132e033649dbbd050765bf7;
+const getRecepies = async (ingredients) => {
+  const APP_ID = 'c34ab5d9';
+  const APP_KEY = 'a7a5284e0132e033649dbbd050765bf7';
   const URL_BASE = 'https://api.edamam.com/api/recipes/v2?type=public';
+
+  const caloriesAmount = calculateCalories();
 
   const url = `${URL_BASE}&q=${encodeURIComponent(
     ingredients
