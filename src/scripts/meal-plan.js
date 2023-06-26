bd0e5a71c8f8250426652a4c15611ca3c82aff5b
alpari01 <alan.parik456@gmail.com>
2023-06-26 18:00:47 +0300

    Meal-plan style update

--- src/scripts/meal-plan.js
+++ src/scripts/meal-plan.js
@@ -25,20 +25,21 @@ const loadMealTypeFromLocalStorage = (mealType) => {
       })
     );
 
-    const mealPlanContent = Object.assign(document.createElement('div'), {
-      className: 'meal-plan-content',
-    });
-    mealContainer.appendChild(mealPlanContent);
-
-    mealPlanContent.appendChild(
+    mealContainer.appendChild(
       Object.assign(document.createElement('div'), {
         className: 'meal-plan-container-left',
         innerHTML: `<span>Calories: ${mealJson['recipe']['calories']}</span><span>Cuisine type: ${mealJson['recipe']['cuisineType']}</span>`,
       })
     );
 
+    const mealPlanContent = Object.assign(document.createElement('div'), {
+      className: 'meal-plan-content',
+    });
+    mealContainer.appendChild(mealPlanContent);
+
     const ingredients = Object.assign(document.createElement('div'), {
       className: 'meal-plan-container-right',
+      innerHTML: '<span class="ingredients-label">Ingredients</span>',
     });
     for (let ingredient of mealJson['recipe']['ingredientLines']) {
       ingredients.innerHTML += `<span>${ingredient}</span>`;
@@ -55,18 +56,34 @@ const loadMealTypeFromLocalStorage = (mealType) => {
     const digest = Object.assign(document.createElement('div'), {
       className: 'meal-digest-container',
     });
+
     digest.appendChild(
       Object.assign(document.createElement('button'), {
         className: 'accordion',
         innerHTML: 'Meal digest',
       })
     );
+
     const panel = Object.assign(document.createElement('div'), {
       className: 'panel',
     });
+
+    const digestTable = document.createElement('table');
+    const headerRow = digestTable.insertRow();
+    const labelHeader = headerRow.insertCell();
+    labelHeader.innerHTML = 'Label';
+    const amountHeader = headerRow.insertCell();
+    amountHeader.innerHTML = 'Amount';
+
     for (let elem of mealJson['recipe']['digest']) {
-      panel.innerHTML += `<span>${elem['label']}: ${elem['total']}mg</span>`;
+      const row = digestTable.insertRow();
+      const labelCell = row.insertCell();
+      labelCell.innerHTML = elem['label'];
+      const amountCell = row.insertCell();
+      amountCell.innerHTML = elem['total'];
     }
+
+    panel.appendChild(digestTable);
     digest.appendChild(panel);
     mealContainer.appendChild(digest);
 
